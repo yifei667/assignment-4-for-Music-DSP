2a8bd5b0bb05a7fdf442a265cdc39bf551dcf986
yifei667 <48230897+yifei667@users.noreply.github.com>
2019-04-13 16:08:17 -0400

    Add files via upload

--- mySinglePole.m
+++ mySinglePole.m
@@ -1,9 +1,10 @@
 function [outputvector] = mySinglePole(inputvector, alpha)
  n=length(inputvector);
+ outputvector=zeros(n,1);
  for i=2:n
    outputvector(i) = (1-alpha)*inputvector(i)+alpha*outputvector(i-1);
  end
- outputvector(1)=inputvector(1);
+ outputvector(1)=(1-alpha)*inputvector(1);
  
 
 
