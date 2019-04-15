3d019f3261b26b1c2397e64bea084d2f1dd542ff
yifei667 <48230897+yifei667@users.noreply.github.com>
2019-04-14 23:05:20 -0400

    Add files via upload

--- ass4main.m
+++ ass4main.m
@@ -29,6 +29,10 @@ title('filter(alpha = 0.99)')
 subplot(2,2,4)
 pwelch(filter4)
 title('filter(alpha = 0.999)')
+audiowrite('filter1.wav',filter1,fs);
+audiowrite('filter2.wav',filter2,fs);
+audiowrite('filter3.wav',filter3,fs);
+audiowrite('filter4.wav',filter4,fs);
 figure(3)
 alpha1=0.9;
 freqz((1-alpha1), [1  -alpha1],1024,44100) 
@@ -37,11 +41,37 @@ figure(4)
 alpha2=0.99;
 freqz((1-alpha2), [1  -alpha2],1024,44100)
 title('frequency response of the filter(alpha=0.99)')
-
 noisefilter= myPeak(x,3000,4, 12, 44100);
+figure(5)
+subplot(2,1,1)
+pwelch(noisefilter)
+title('frequency response of mypeak (noise)')
 cathy_2filter= myPeak(cathy_2,3000,4, 12, 44100);
+subplot(2,1,2)
+pwelch(cathy_2filter)
+title('frequency response of mypeak(cathy_2)')
+
 noise_filter2=my2ndorder(x,3000, 4, 12, 44100);
-diff1=noise_filter2-noisefilter;
+figure(6)
+subplot(2,1,1)
+pwelch(noise_filter2)
+title('frequency response of my2ndorder (noise)')
 cathy_2filter2=my2ndorder(cathy_2,3000,4, 12, 44100);
+subplot(2,1,2)
+pwelch(cathy_2filter2)
+title('frequency response of my2ndorder(cathy_2)')
+diff1=noise_filter2-noisefilter;
 diff2=cathy_2filter2-cathy_2filter;
+figure(7)
+subplot(2,1,1)
+plot(diff1)
+title('diff for the my2ndorder and mypeak for noise');
+xlabel('number of samples')
+ylabel('magnitude')
+subplot(2,1,2)
+plot(diff2)
+title('diff for the my2ndorder and mypeak for cathy_2')
+xlabel('number of samples')
+ylabel('magnitude')
+
 
